9d0b587bf909de2d0262fba67baf2167f8547916
Joshua Thompson <jtwork1995@gmail.com>
2025-08-10 15:30:36 -0400

    added a javascript file to play audio on click; added placeholder audio .ogg and .wav; reorganized site structure.

---
+++ res/scripts/gojuuon_playback.js
@@ -0,0 +1,63 @@
+
+sound_a = new Audio("../../res/audio/coin.ogg");
+sound_i = new Audio("../../res/audio/coin.ogg");
+sound_u = new Audio("../../res/audio/coin.ogg");
+sound_e = new Audio("../../res/audio/coin.ogg");
+sound_o = new Audio("../../res/audio/coin.ogg");
+
+sound_ka = new Audio("../../res/audio/coin.ogg");
+sound_ki = new Audio("../../res/audio/coin.ogg");
+sound_ku = new Audio("../../res/audio/coin.ogg");
+sound_ke = new Audio("../../res/audio/coin.ogg");
+sound_ko = new Audio("../../res/audio/coin.ogg");
+
+sound_sa = new Audio("../../res/audio/coin.ogg");
+sound_shi = new Audio("../../res/audio/coin.ogg");
+sound_su = new Audio("../../res/audio/coin.ogg");
+sound_se = new Audio("../../res/audio/coin.ogg");
+sound_so = new Audio("../../res/audio/coin.ogg");
+
+sound_ta = new Audio("../../res/audio/coin.ogg");
+sound_chi = new Audio("../../res/audio/coin.ogg");
+sound_tsu = new Audio("../../res/audio/coin.ogg");
+sound_te = new Audio("../../res/audio/coin.ogg");
+sound_to = new Audio("../../res/audio/coin.ogg");
+
+sound_ma = new Audio("../../res/audio/coin.ogg");
+sound_mi = new Audio("../../res/audio/coin.ogg");
+sound_mu = new Audio("../../res/audio/coin.ogg");
+sound_me = new Audio("../../res/audio/coin.ogg");
+sound_mo = new Audio("../../res/audio/coin.ogg");
+
+sound_ha = new Audio("../../res/audio/coin.ogg");
+sound_hi = new Audio("../../res/audio/coin.ogg");
+sound_fu = new Audio("../../res/audio/coin.ogg");
+sound_he = new Audio("../../res/audio/coin.ogg");
+sound_ho = new Audio("../../res/audio/coin.ogg");
+
+sound_na = new Audio("../../res/audio/coin.ogg");
+sound_ni = new Audio("../../res/audio/coin.ogg");
+sound_nu = new Audio("../../res/audio/coin.ogg");
+sound_ne = new Audio("../../res/audio/coin.ogg");
+sound_no = new Audio("../../res/audio/coin.ogg");
+
+sound_ya = new Audio("../../res/audio/coin.ogg");
+sound_yu = new Audio("../../res/audio/coin.ogg");
+sound_yo = new Audio("../../res/audio/coin.ogg");
+
+sound_ra = new Audio("../../res/audio/coin.ogg");
+sound_ri = new Audio("../../res/audio/coin.ogg");
+sound_ru = new Audio("../../res/audio/coin.ogg");
+sound_re = new Audio("../../res/audio/coin.ogg");
+sound_ro = new Audio("../../res/audio/coin.ogg");
+
+sound_wa = new Audio("../../res/audio/coin.ogg");
+sound_wo = new Audio("../../res/audio/coin.ogg");
+sound_n = new Audio("../../res/audio/coin.ogg");
+
+function play_kana_sound(src_sound) {
+	src_sound.play();
+	//alert(src_sound.src);
+}
+
+
